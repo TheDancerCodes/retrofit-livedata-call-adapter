3b60b49678e53110474b81b4e9492cc2f8a378f4
Roger Taracha <rojtaracha@gmail.com>
2020-04-25 18:33:00 +0300

    Dependency Reference Restructure: * Delete app module and update some documentation.

--- sample-app/src/main/java/com/thedancercodes/sample_app/network/PostService.kt
+++ sample-app/src/main/java/com/thedancercodes/sample_app/network/PostService.kt
@@ -5,6 +5,11 @@ import com.thedancercodes.livedatalibrary.ApiResponse
 import com.thedancercodes.sample_app.model.Post
 import retrofit2.http.GET
 
+/**
+ * Retrofit turns your HTTP API into a Kotlin interface
+ *
+ * NOTE: PostService methods use LiveData as their return type.
+ */
 interface PostService {
 
     @GET("posts")
